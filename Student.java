7fcd10d6b535ad2db3c346bee9fffe7adfbd5729
Niharika Kanchi <niharikakanchi2659@gmail.com>
2026-07-31 23:12:42 +0930

    Improve Student.java documentation

---
+++ Student.java
@@ -0,0 +1,29 @@
+public class Student {
+
+    private int studentId;
+    private String name;
+    private String course;
+
+    public Student(int studentId, String name, String course) {
+        this.studentId = studentId;
+        this.name = name;
+        this.course = course;
+    }
+
+    public void displayStudentDetails() {
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Name: " + name);
+        System.out.println("Course: " + course);
+    }
+
+    public static void main(String[] args) {
+
+        Student student = new Student(
+            123,
+            "Niharika",
+            "Master of Software Engineering"
+        );
+
+        student.displayStudentDetails();
+    }
+}
