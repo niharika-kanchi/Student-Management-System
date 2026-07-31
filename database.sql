f4a64dae2721f6e30d6bd05b3353d5102e489d12
Niharika Kanchi <niharikakanchi2659@gmail.com>
2026-07-31 23:31:07 +0930

    add database.sql file

--- database.sql
+++ database.sql
@@ -5,7 +5,6 @@ USE student_management;
 CREATE TABLE students (
     student_id INT PRIMARY KEY,
     name VARCHAR(100),
-    room VARCHAR(100),
     email VARCHAR(100),
     course VARCHAR(100)
 );
