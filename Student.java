9fcaf95b6971909a3b2e3e2aea23105477a0ace8
Niharika Kanchi <niharikakanchi2659@gmail.com>
2026-08-01 00:11:14 +0930

    update student.java

--- Student.java
+++ Student.java
@@ -19,7 +19,7 @@ public class Student {
     public static void main(String[] args) {
 
         Student student = new Student(
-            123,
+            101,
             "Niharika",
             "Master of Software Engineering"
         );
