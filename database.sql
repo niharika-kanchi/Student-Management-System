8518374f1caadc10ac75ba8dc9726f7df82e9c7a
Niharika Kanchi <niharikakanchi2659@gmail.com>
2026-07-31 23:44:44 +0930

    add information in student records

--- database.sql
+++ database.sql
@@ -9,3 +9,10 @@ CREATE TABLE students (
     course VARCHAR(100),    
     dob DATE
 );
+
+INSERT INTO students (
+(student_id, name, email, course, dob)
+VALUES
+(101, 'Isha Hi', 'Hi@email.com', 'Master of Teaching', '2000-05-15'),
+(102, 'Hello Miss', 'hello@email.com', 'Computer Science', '1999-08-20');
+);
